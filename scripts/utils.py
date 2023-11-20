e88b096d89160dedda4c71ca3264f3f124c96de5
Stefan Pate <stefanpate94@gmail.com>
2023-11-20 06:51:35 -0600

    Updates to make_pathways while analyzing 4 step succ to mvl ha precursor.

---
+++ scripts/utils.py
@@ -0,0 +1,19 @@
+import json
+from rdkit import Chem
+
+def save_json(data, save_to):
+    with open(save_to, 'w') as f:
+        json.dump(data, f)
+    
+def load_json(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def sanitize(list_of_smiles):
+    sanitized_smiles = []
+    for elt in list_of_smiles:
+        temp_mol = Chem.MolFromSmiles(elt)
+        Chem.rdmolops.RemoveStereochemistry(temp_mol)
+        sanitized_smiles.append(Chem.MolToSmiles(temp_mol))    
+    return sanitized_smiles

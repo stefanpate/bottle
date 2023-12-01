ec72cbe9ac96be6c6bbba37f8c796c4c2c07976b
Stefan Pate <stefanpate94@gmail.com>
2023-12-01 17:23:07 -0600

    Began developing peri-reaction-center-MCS code in rxn_ctr_mcs notebook

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,6 +11,10 @@ def load_json(path):
     return data
 
 def sanitize(list_of_smiles):
+    '''
+    Remove stereochem and canonicalize
+    a list of smiles
+    '''
     sanitized_smiles = []
     for elt in list_of_smiles:
         temp_mol = Chem.MolFromSmiles(elt)

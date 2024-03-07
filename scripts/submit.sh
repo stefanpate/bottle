67358175fbaf3df01cc2716b2974e425b8892233
stefanpate <stefanpate94@gmail.com>
2024-03-07 08:10:29 -0600

    Ran updated mcs enzyme validation on 50k reaction pairs

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,8 +3,8 @@
 #SBATCH -p b1039
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=16G
-#SBATCH -t 48:00:00
+#SBATCH --mem=4G
+#SBATCH -t 24:00:00
 #SBATCH --job-name="val_kr_uniprot"
 #SBATCH -o ../logs/outlog_validation_jni_uniprot
 #SBATCH -e ../logs/errlog_validation_jni_uniprot

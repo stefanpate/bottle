ad501a107801b2f91348d26b437f3ae475175a2d
stefanpate <stefanpate94@gmail.com>
2024-04-16 15:32:01 -0500

    Fixed mislabeling of akg semialdehyde starter

--- scripts/run_pickaxe.py
+++ scripts/run_pickaxe.py
@@ -4,7 +4,7 @@ os.chdir(CWD)
 
 from minedatabase.pickaxe import Pickaxe
 from minedatabase.utils import get_compound_hash
-from minedatabase.rules import metacyc_intermediate
+from minedatabase.rules import metacyc_intermediate, metacyc_generalized
 from minedatabase.filters import (
     SimilarityFilter,
     SimilaritySamplingFilter,
@@ -14,11 +14,12 @@ from rdkit.Chem import CanonSmiles
 
 # Directories and files
 st_dir = "../data/starters_targets/"
-input_cpds = "alpha_ketoglutarate_semialdehyde"
-target_cpds = "hopa"
+input_cpds = "alpha_ketoglutarate"
+target_cpds = "alpha_ketoglutarate_semialdehyde"
 input_cpds_fn = st_dir + input_cpds + ".csv"
 target_cpds_fn = st_dir + target_cpds + ".csv"
 rule_list = '../data/rules/JN3604IMT_rules.tsv'
+# rule_list = '../data/rules/minimal1224_all_uniprot.tsv'
 
 # Pickaxe settings
 processes = 1 # 50
@@ -39,6 +40,10 @@ _, coreactant_list, rule_name = metacyc_intermediate(
     fraction_coverage=1
     # n_rules=5
 )
+
+# _, coreactant_list, rule_name = metacyc_generalized(
+#     fraction_coverage=1
+# )
 
 pk = Pickaxe(
         coreactant_list=coreactant_list,

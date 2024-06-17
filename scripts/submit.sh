b255e8c55ccc8f1674df94f1f22cb0fdb30d58ae
stefanpate <stefanpate94@gmail.com>
2024-06-17 13:38:55 -0500

    akg -> hopa with carbonyl free rule set. figures for bottle workshop

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -A b1039
-#SBATCH -p b1039
+#SBATCH -A p30041
+#SBATCH -p normal
 #SBATCH -N 1
 #SBATCH -n 50
 #SBATCH --mem=0
 #SBATCH -t 04:00:00
-#SBATCH --job-name="2genccm2hopa"
+#SBATCH --job-name="2genakg2hopa"
 #SBATCH -o ../logs/outlog
 #SBATCH -e ../logs/errlog
 ulimit -c 0
 module load python/anaconda3.6
 source activate mine
-python -u validate_known_rxn_uniprot_mcs.py
+python -u run_pickaxe.py

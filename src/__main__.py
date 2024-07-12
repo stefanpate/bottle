691dc94e6b1c9424fe7d5612d1c9c15cd9adc36a
Pavel Katsev <pkatsev@gmail.com>
2024-07-12 11:28:43 -0500

    switching to better cli lib

--- src/__main__.py
+++ src/__main__.py
@@ -1,47 +1,58 @@
 import sys
+from contextlib import nullcontext
 from typing import Annotated
 
-import click
-import typer
+from cyclopts import App, Group, Parameter, validators
+from cyclopts.types import File
 from rdkit import Chem
 
 from src.smarts import MoleculeSmarts
-from src.utils import filter_smiles_by_smarts, ensure_type
+from src.utils import ensure_type
 
-app = typer.Typer(no_args_is_help=True)
+app = App("Bottle CLI")
 
-MolSmartsChoice = Annotated[
-    str,
-    typer.Option(click_type=click.Choice([mem.name for mem in MoleculeSmarts]))
-]
+_smarts_group = Group(
+    'smarts (choose one)',
+    default_parameter=Parameter(negative=""),  # Disable "--no-" flags
+    validator=validators.LimitedChoice(min=1),  # Mutually Exclusive Options
+)
 
 
 @app.command()
 def filter_smiles(
-        in_smiles: Annotated[typer.FileText, typer.Argument()] = sys.stdin,
-        out_smiles: Annotated[typer.FileTextWrite, typer.Argument()] = sys.stdout,
+        in_smiles: Annotated[File, Parameter(name=('--input', '-I'))] = None,
         *,
-        smarts: str = None,
-        mol_smarts: MolSmartsChoice = None,
+        out_smiles: Annotated[File, Parameter(name=('--output', '-O'))] = None,
+        smarts: Annotated[str, Parameter(group=_smarts_group)] = None,
+        mol_smarts: Annotated[MoleculeSmarts, Parameter(group=_smarts_group)] = None,
 ):
-    if not (smarts := smarts or (MoleculeSmarts[mol_smarts] if mol_smarts else None)):
-        raise typer.BadParameter("You must provide exactly one of --smarts or --mol_smarts.")
+    """Filter a stream of SMILES lines by a SMARTS pattern.
 
-    smarts = ensure_type(smarts, Chem.Mol, factory=Chem.MolFromSmarts)
-    for line_num, line in enumerate(in_smiles, start=1):
-        if not (smiles := ensure_type(line.strip(), Chem.Mol, factory=Chem.MolFromSmiles)):
-            print(f'could not parse smiles on line {line_num}', file=sys.stderr)
-            continue
+    Args:
+        in_smiles: input stream of SMILES lines
+        out_smiles: output stream of SMILES lines
+        smarts: free-form SMARTS pattern
+        mol_smarts: predefined SMARTS patterns
+    """
+    smarts_str = mol_smarts or smarts
+    smarts = ensure_type(smarts_str, Chem.Mol, factory=Chem.MolFromSmarts)
 
-        if smiles.HasSubstructMatch(smarts):
-            out_smiles.write(line)
+    with (
+        in_smiles.open(mode='r') if in_smiles else nullcontext(sys.stdin) as fin,
+        out_smiles.open(mode='w') if out_smiles else nullcontext(sys.stderr) as fout,
+    ):
+        for line_num, line in enumerate(fin, start=1):
+            if not (smiles := ensure_type(line.strip(), Chem.Mol, factory=Chem.MolFromSmiles)):
+                print(f'could not parse smiles on line {line_num}', file=sys.stderr)
+                continue
+
+            if smiles.HasSubstructMatch(smarts):
+                fout.write(line)
 
 
-@app.callback()
-def callback():
-    """Dummy callback function. Remove when >1 subcommands."""
-    pass
+def main():
+    app()
 
 
 if __name__ == "__main__":
-    app()
+    main()

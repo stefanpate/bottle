8cfd4361ed2ba9c305fd4eb2838296132390c3b0
stefanpate <stefanpate94@gmail.com>
2024-07-27 17:17:56 -0500

    Pulled rhea smiles names

--- scripts/download_rhea.py
+++ scripts/download_rhea.py
@@ -1,38 +1,6 @@
-# import requests
-
-# with open("../artifacts/rheas_to_get_names.txt", 'r') as f:
-#     rheas_to_get = [line.strip() for line in f]
-
-# def get_from_mol_elt(mol_elt, keys=('smiles', 'label')):
-#     return {k: mol_elt[k] for k in keys}
-
-# def get_from_side(side, keys=('smiles', 'label')):
-#     mol_pulls = []
-#     for elt in side:
-#         if 'reactivePart' in elt:
-#             for mol_elt in elt['reactivePart']:
-#                 mol_pulls.append(get_from_mol_elt(mol_elt, keys))
-#         else:
-#             mol_pulls.append(get_from_mol_elt(elt, keys))
-#     return mol_pulls
-
-# def get_from_rxn_response(response, keys=('smiles', 'label')):
-#     pull = {}
-#     pull['left'] = get_from_side(response['left'], keys)
-#     pull['right'] = get_from_side(response['right'], keys)
-#     return pull
-
-
-# for i, rid in enumerate(rheas_to_get):
-#     url = f"https://www.rhea-db.org/rhea/{rid}/json"
-#     response = requests.get(url).json()
-#     print(get_from_rxn_response(response))
-
-
 import requests
 import multiprocessing as mp
 import json
-import time
 
 with open("../artifacts/rheas_to_get_names.txt", 'r') as f:
     rheas_to_get = [line.strip() for line in f]
@@ -61,15 +29,8 @@ def get_from_rxn_rid(rid, keys=('smiles', 'label')):
     except:
         return None
 
-n = 100
-rheas_to_get = rheas_to_get[:n]
-
-tic = time.perf_counter()
 with mp.Pool() as pool:
     res = pool.map(get_from_rxn_rid, rheas_to_get)
-
-toc = time.perf_counter()
-print(f"{n} in {toc - tic}s")
 
 res = dict(zip(rheas_to_get, res))
 
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#SBATCH -A p30041
-#SBATCH -p normal
+#SBATCH -A b1039
+#SBATCH -p b1039
 #SBATCH -N 1
 #SBATCH -n 50
-#SBATCH --mem=0
-#SBATCH -t 04:00:00
-#SBATCH --job-name="2genakg2hopa"
+#SBATCH --mem=16GB
+#SBATCH -t 08:00:00
+#SBATCH --job-name="rhea_smi_name"
 #SBATCH -o ../logs/outlog
 #SBATCH -e ../logs/errlog
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=stefan.pate@northwestern.edu
 ulimit -c 0
 module load python/anaconda3.6
 source activate mine
-python -u run_pickaxe.py
+python -u download_rhea.py

3ef1d46b06b3ac3cf734be8aa6f80b826298400d
stefanpate <stefanpate94@gmail.com>
2024-08-12 16:19:15 -0500

    Ran pickaxe, process expansions, and examples notebook with python=3.9

--- scripts/run_pickaxe.py
+++ scripts/run_pickaxe.py
@@ -12,31 +12,31 @@ from minedatabase.filters import (
 # Directories and files
 st_dir = "../data/starters_targets/"
 input_cpds = "alpha_ketoglutarate"
-target_cpds = "hopa"
+target_cpds = "alpha_ketoglutarate_semialdehyde"
 input_cpds_fn = st_dir + input_cpds + ".csv"
 target_cpds_fn = st_dir + target_cpds + ".csv"
 rule_list = '../data/rules/JN3604IMT_rules_carbonyl_free.tsv'
 coreactant_list = "../data/rules/metacyc_coreactants_carbonyl_free.tsv"
 
 # Pickaxe settings
-processes = 50 # 50
+processes = 4 # 50
 generations = 2
 
 tani_filter = False
 tani_threshold = [0, 0, 0.3, 0.3, 0.3]
 increasing_tani = False
 
-tani_sample = False # True
+tani_sample = True # True
 sample_size = 1000 # 1000
 weight = None # 5
 
-save_to = f"/projects/b1039/spn1560/bottle/{input_cpds}_to_{target_cpds}_gen_{generations}_tan_sample_{int(tani_sample)}_n_samples_{sample_size}.pk"
+# save_to = f"/projects/b1039/spn1560/bottle/{input_cpds}_to_{target_cpds}_gen_{generations}_tan_sample_{int(tani_sample)}_n_samples_{sample_size}.pk"
+save_to = f"../data/raw_expansions/{input_cpds}_to_{target_cpds}_gen_{generations}_tan_sample_{int(tani_sample)}_n_samples_{sample_size}.pk"
 
-
-_, _, rule_name = metacyc_intermediate(
-    fraction_coverage=1
+# _, _, rule_name = metacyc_intermediate(
+#     fraction_coverage=1
     # n_rules=5
-)
+# )
 
 # _, coreactant_list, rule_name = metacyc_generalized(
 #     fraction_coverage=1

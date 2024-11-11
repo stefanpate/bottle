c62aeb3f0954c254f379ecab4740869ecc9997df
stefanpate <stefanpate94@gmail.com>
2024-11-10 23:57:47 -0600

    Downgraded rdkit. Was trouble finding it with poetry or pipx not sure

--- scripts/slurm/expand.sh
+++ scripts/slurm/expand.sh
@@ -10,16 +10,16 @@
 #SBATCH --error=/path/to/error/logs/%A
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=<YOUR EMAIL>
+#SBATCH --mail-user=stefan.pate@northwestern.edu
 
 # Args
-scripts_dir=/path/to/your/scripts
-starters=foo
-generations=1
-targets=bar
+scripts_dir=/home/spn1560/bottle/scripts
+starters=bottle_targets_24
+generations=2
+processes=50
 
 # Commands
 ulimit -c 0
 module purge
-source /path/to/your/environment/bin/activate
-python $scripts_dir/run_pickaxe.py $starters $generations
+source /home/spn1560/.cache/pypoetry/virtualenvs/bottle-OcfYl-TY-py3.12/bin/activate
+python $scripts_dir/run_pickaxe.py $starters $generations -p $processes

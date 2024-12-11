2c4f7137984e8a9013177338f1d496884e16dc73
stefanpate <stefanpate94@gmail.com>
2024-12-11 11:13:23 -0600

    qlogs

--- scripts/slurm/expand.sh
+++ scripts/slurm/expand.sh
@@ -2,9 +2,9 @@
 #SBATCH -A p30041
 #SBATCH -p normal
 #SBATCH -N 1
-#SBATCH -n 40
+#SBATCH -n 50
 #SBATCH --mem=0
-#SBATCH -t 05:00:00
+#SBATCH -t 3:00:00
 #SBATCH --job-name="exp"
 #SBATCH --output=/home/spn1560/bottle/logs/expand/out/%A
 #SBATCH --error=/home/spn1560/bottle/logs/expand/error/%A
@@ -14,12 +14,12 @@
 
 # Args
 scripts_dir=/home/spn1560/bottle/scripts
-starters=ccm_aa
+starters=bottle_targets_24
 generations=2
-processes=40
+processes=50
 
 # Commands
 ulimit -c 0
 module purge
 source /home/spn1560/.cache/pypoetry/virtualenvs/bottle-OcfYl-TY-py3.12/bin/activate
-python $scripts_dir/run_pickaxe.py $starters $generations -p $processes --a-plus-b
+python $scripts_dir/run_pickaxe.py $starters $generations -p $processes

04c9783bdd3c7f8bff0ef15be3a68a580fb9b6f8
stefanpate <stefanpate94@gmail.com>
2025-06-06 13:14:14 -0500

    Round of expansions for all hands

--- scripts/slurm/expand.sh
+++ scripts/slurm/expand.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1
 #SBATCH -n 50
 #SBATCH --mem=0
-#SBATCH -t 3:00:00
+#SBATCH -t 4:00:00
 #SBATCH --job-name="exp"
 #SBATCH --output=/home/spn1560/bottle/logs/expand/out/%A
 #SBATCH --error=/home/spn1560/bottle/logs/expand/error/%A
@@ -14,15 +14,16 @@
 
 # Args
 scripts_dir=/home/spn1560/bottle/scripts
-starters=alpha_ketoglutarate
-targets=L-glutamine
-generations=2
+starters=bottle_targets_24
+targets=null
+generations=4
 processes=50
-a_plus_b=False
+a_plus_b=True
 rules=mechinferred_dt_98_rules_w_coreactants
+prune=False
 
 # Commands
 ulimit -c 0
 module purge
-source /home/spn1560/.cache/pypoetry/virtualenvs/bottle-OcfYl-TY-py3.12/bin/activate
-python $scripts_dir/expand.py starters=$starters targets=$targets generations=$generations rules=$rules processes=$processes
+source /home/spn1560/.cache/pypoetry/virtualenvs/bottle-jRVXeMfS-py3.12/bin/activate
+python3 $scripts_dir/expand.py starters=$starters targets=$targets generations=$generations rules=$rules processes=$processes a_plus_b=$a_plus_b prune_to_targets=$prune

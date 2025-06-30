a116e766e0a904adde0e11791ade182e7393e952
stefanpate <stefanpate94@gmail.com>
2025-06-30 15:08:34 -0500

    conf for mechanism proc on quest

--- scripts/slurm/mechanism_processing.sh
+++ scripts/slurm/mechanism_processing.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1
 #SBATCH -n 50
 #SBATCH --mem=0
-#SBATCH -t 8:00:00
+#SBATCH -t 3:00:00
 #SBATCH --job-name="mech_proc"
 #SBATCH --output=/home/spn1560/bottle/logs/mech_proc/out/%A
 #SBATCH --error=/home/spn1560/bottle/logs/mech_proc/error/%A

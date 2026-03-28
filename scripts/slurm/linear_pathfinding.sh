16714ef327b683129358fb7bd537b5f747a9391e
stefanpate <stefanpate94@gmail.com>
2026-03-28 12:25:54 -0500

    tweak slurm scripts, start fixing up equilibrator patch

--- scripts/slurm/linear_pathfinding.sh
+++ scripts/slurm/linear_pathfinding.sh
@@ -3,8 +3,8 @@
 #SBATCH -p short
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=10GB
-#SBATCH -t 1:00:00
+#SBATCH --mem=24GB
+#SBATCH -t 4:00:00
 #SBATCH --job-name="lin_path"
 #SBATCH --output=/home/spn1560/bottle/logs/out/%A
 #SBATCH --error=/home/spn1560/bottle/logs/error/%A
@@ -14,12 +14,12 @@
 
 # Args
 script=/home/spn1560/bottle/scripts/linear_pathfinding.py
-expansion=3_steps_bottle_targets_25_to_None_rules_mechinferred_dt_04_rules_w_coreactants_co_metacyc_coreactants_sampled_False_pruned_False_aplusb_False
-casp_study=perf
-max_depth=3
+forward_expansion=2026-03-26/17-52-16
+retro_expansion=2026-03-26/17-54-12
+casp_study=260327_target_3hpa
 
 # Commands
 ulimit -c 0
 module purge
 source /home/spn1560/.cache/pypoetry/virtualenvs/bottle-jRVXeMfS-py3.12/bin/activate
-python $script max_depth=$max_depth expansion=$expansion casp_study=$casp_study
+python $script forward_expansion=$forward_expansion retro_expansion=$retro_expansion casp_study=$casp_study
--- scripts/slurm/process_paths.sh
+++ scripts/slurm/process_paths.sh
@@ -2,9 +2,9 @@
 #SBATCH -A b1039
 #SBATCH -p b1039
 #SBATCH -N 1
-#SBATCH -n 50
-#SBATCH --mem=0
-#SBATCH -t 4:00:00
+#SBATCH -n 10
+#SBATCH --mem=32GB
+#SBATCH -t 8:00:00
 #SBATCH --job-name="path_proc"
 #SBATCH --output=/home/spn1560/bottle/logs/path_proc/out/%A
 #SBATCH --error=/home/spn1560/bottle/logs/path_proc/error/%A
@@ -14,13 +14,13 @@
 
 # Args
 scripts_dir=/home/spn1560/bottle/scripts
-processes=50 # Make sure this matches #SBATCH -n
-casp_study=bottle25
+processes=10 # Make sure this matches #SBATCH -n
+casp_study=260327_target_3hpa
 
 # Commands
 ulimit -c 0
 module purge
 source /home/spn1560/.cache/pypoetry/virtualenvs/bottle-jRVXeMfS-py3.12/bin/activate
-python -u $scripts_dir/analyze_structures.py processes=$processes casp_study=$casp_study
+# python -u $scripts_dir/analyze_structures.py processes=$processes casp_study=$casp_study
 python -u $scripts_dir/analyze_thermo.py casp_study=$casp_study
-python -u $scripts_dir/draw_reactions.py casp_study=$casp_study
+# python -u $scripts_dir/draw_reactions.py casp_study=$casp_study
